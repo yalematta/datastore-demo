1909c9759741f5dcbb2b90146d6b8cefc7637b26
yalematta <layale.matta@gmail.com>
2021-05-11 13:41:30 +0300

    add DataStoreRepository

---
+++ app/src/main/java/com/yalematta/datastore_demo/repository/DataStoreRepository.kt
@@ -0,0 +1,58 @@
+package com.yalematta.datastore_demo.repository
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.*
+import com.yalematta.datastore_demo.repository.DataStoreRepository.PreferencesKeys.REMEMBER
+import com.yalematta.datastore_demo.repository.DataStoreRepository.PreferencesKeys.USERNAME
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+data class UserPreferences(
+    val username: String,
+    val remember: Boolean
+)
+
+class DataStoreRepository(private val dataStore: DataStore<Preferences>) {
+
+    private object PreferencesKeys {
+        val USERNAME = stringPreferencesKey("username")
+        val REMEMBER = booleanPreferencesKey("remember")
+    }
+
+    suspend fun saveToDataStore(username: String, remember: Boolean) {
+        dataStore.edit { preference ->
+            preference[USERNAME] = username
+            preference[REMEMBER] = remember
+        }
+    }
+
+    val readFromDataStore : Flow<UserPreferences> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                Log.d(DataStoreRepository::class.java.simpleName, exception.message.toString())
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preference ->
+            val username = preference[USERNAME] ?: ""
+            val remember = preference[REMEMBER] ?: false
+            UserPreferences(username, remember)
+        }
+
+    suspend fun clearDataStore() {
+        dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
+
+    suspend fun removeUsername() {
+        dataStore.edit { preference ->
+            preference.remove(USERNAME)
+        }
+    }
+}
